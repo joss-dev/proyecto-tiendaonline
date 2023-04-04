bb0f7a94e5b2ce8a0302e568acbd0608ef1201e8
jossdev <josekandia42@gmail.com>
2023-04-04 15:36:54 +0200

    first commit

---
+++ app/Views/probando.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="./public/css/bootstrap.css">
+  </head>
+  <body>
+    <script src="./public/js/bootstrap.bundle.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  </body>
+</html>
