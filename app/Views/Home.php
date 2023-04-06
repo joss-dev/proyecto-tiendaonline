163511c1dc2b26a2ef10caacf8949cb333c4f65a
jossdev <josekandia42@gmail.com>
2023-04-06 19:23:29 +0200

    elimina normalize.css

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
   <link rel="stylesheet" href="./public/css/bootstrap.css">
+  <link rel="stylesheet" href="./public/css/myStyle.css">
 </head>
 
 <body>
