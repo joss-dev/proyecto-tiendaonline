00d32d1b2875e967c2bbffffc5f17400a2f10953
jossdev <josekandia42@gmail.com>
2023-04-06 23:49:14 +0200

    agrega navbar

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -4,19 +4,66 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link rel="stylesheet" href="./public/css/bootstrap.css">
+  <title>TecnoPhone</title>
+  <link rel="stylesheet" href="./public/css/bootstrap.min.css">
   <link rel="stylesheet" href="./public/css/myStyle.css">
 </head>
 
 <body>
-  <div class="container-fluid">
-    <nav class="navbar navbar-dark bg-dark">
-      <!-- Navbar content -->
-    </nav>
-  </div>
-
-  <script src="./public/js/bootstrap.bundle.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  <nav class="navbar navbar-light navbar-expand-lg" style="background-color: #03001c;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./public/img/icon.png" alt="" width="50" height="60">
+      </a>
+      <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mx-6 mb-3 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#">Principal</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Quienes Somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Terminos y Usos</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              La empresa
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Contacto</a></li>
+              <li><a class="dropdown-item" href="#">Comercialización</a></li>
+              <li><a class="dropdown-item" href="#">Consultas</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control mx-4" type="search" placeholder="Buscar" aria-label="Buscar">
+          <button class="btn btn-outline-light" type="submit">Buscar</button>
+        </form>
+      </div>
+      <ul class="navbar-bar d-flex flex-row flex-wrap">
+        <li class="nav-item col-6 col-md-6 p-4">
+          <a class="" href="#">
+            <img src="./public/img/icons/carrito.png" alt="" width="30" height="30">
+          </a>
+        </li>
+        <li class="nav-item col-6 col-md-6 p-4">
+          <a class="" href="#">
+            <img src="./public/img/icons/usuario.png" alt="" width="30" height="30">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 
 </html>
