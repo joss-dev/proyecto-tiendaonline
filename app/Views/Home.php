15e17324a0f8546ad635fe9ac550d2ee585cbca9
jossdev <josekandia42@gmail.com>
2023-04-07 01:08:09 +0200

    añade icons y footer

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -62,6 +62,32 @@
       </ul>
     </div>
   </nav>
+  <div class="container-fluid">
+    <h1>hola</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis debitis illum iste, commodi ex culpa iusto similique a ut deleniti perspiciatis aperiam in eius maxime reiciendis voluptas eaque adipisci! Atque nostrum explicabo, corporis cupiditate suscipit quibusdam earum! Optio animi, quo eius unde inventore ut? Nam est iusto suscipit ratione tempora consequatur, voluptatibus, rem incidunt exercitationem illo sunt iure odit alias, maxime molestias dolorem earum harum neque ad vero. Dolorem animi voluptatem optio quod alias repellendus expedita. Vero dolor eos modi animi molestiae delectus quis fugiat totam at, ipsa minus! Quia amet repellendus officia soluta nesciunt reiciendis nostrum, architecto numquam suscipit molestiae temporibus sed laudantium optio aut expedita. Natus est temporibus consequuntur, perferendis iure ducimus laborum eveniet dicta cum ipsa veritatis aspernatur, aperiam sed sint explicabo expedita minima placeat. Atque ullam consectetur quasi aliquam quo quos ex! Nulla aut provident quisquam, unde libero cumque voluptate possimus vero blanditiis rerum dolores quasi non magnam voluptatibus tempora maxime! Expedita quas, eos perspiciatis, aspernatur ut voluptas ducimus similique odit, sit necessitatibus quidem! Esse sequi maiores qui, ex labore similique accusamus modi nobis repudiandae quia, molestiae numquam quasi doloremque minima dicta commodi cum? Perspiciatis odio officiis architecto quasi explicabo nobis ipsa non perferendis omnis! Nihil in explicabo vero cum maiores, quam suscipit impedit soluta, a consequatur facilis ut corrupti quos tempora tempore nesciunt. Est quam odio at iste facere iusto accusantium in sint, omnis, et ipsum ad veniam quasi corrupti, esse fugiat voluptas voluptate a officia. Eveniet consectetur, eligendi, iste reiciendis, saepe officiis nostrum aliquam error sit explicabo dolore qui voluptates? Expedita, alias ipsum mollitia tenetur, unde voluptatibus culpa atque dolor quos autem quam! Ea veritatis autem dolores consequuntur doloremque voluptate natus fuga facilis cumque voluptates, eaque quaerat ducimus consectetur quos eos, sint laudantium officia facere ut at! Nesciunt eaque, suscipit fuga aperiam illum in laboriosam expedita eligendi saepe, repellendus eius, magni tempora excepturi praesentium. Eligendi dolorem aliquid officia optio, exercitationem laborum nostrum repellendus! Laboriosam at magni nobis corporis autem nostrum, porro modi. Nobis necessitatibus repellendus iure vitae voluptate corporis tempora voluptatem assumenda quisquam, facilis debitis adipisci incidunt similique fugiat repellat tenetur hic harum consequuntur officia nihil voluptatibus ex est doloribus error. Hic, distinctio. Delectus magni consectetur natus, ipsum dolores veritatis maxime atque quisquam totam, voluptate repudiandae neque, itaque sed nesciunt. Nisi, ducimus? Voluptatibus consequatur deserunt aut facilis harum error inventore? Expedita ducimus perferendis voluptatem reprehenderit maxime nostrum voluptas hic, iste, cumque, repellendus adipisci qui doloribus! Aliquam laborum ullam molestiae sapiente asperiores eveniet nostrum consequatur at modi inventore vel quos in odio ad dolorum cum, eum voluptatem quis, incidunt deleniti assumenda? Consequatur atque magni aspernatur expedita obcaecati veritatis ullam debitis molestias officia, nemo, aliquid, doloribus numquam sunt. Necessitatibus, dicta. Recusandae, id illo fuga, neque voluptas laborum facere suscipit atque cumque velit eligendi quo hic voluptatum? Quia quis placeat cum? Vero hic ut aspernatur optio in rem, distinctio neque, nulla quaerat, possimus sequi similique totam ipsa? Nam autem eum neque enim fugit quaerat velit numquam quo, placeat deserunt a harum voluptatibus adipisci laudantium modi aperiam?
+    </p>
+  </div>
+
+  <footer class="text-center text-white" style="background-color: #03001c;">
+    <ul class="row m-0">
+      <li class="col col-sm-12 m-4">Tecno phone</li>
+      <li class="col col-sm-12 m-4">Contacto
+        <ul>
+          <li><span>tecnophone@gmail.com</span></li>
+          <li>3794551245</li>
+          <li>Av. Independencia 2345</li>
+        </ul>
+      </li>
+      <li class="col col-sm-12 m-4">Redes sociales
+      <ul>
+          <li><span>Facebook</span></li>
+          <li>Whatsapp</li>
+          <li>Instagram</li>
+        </ul>
+      </li>
+    </ul>
+  </footer>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
