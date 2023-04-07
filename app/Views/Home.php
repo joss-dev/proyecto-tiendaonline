586aad39c1768f91b2686c3cf2fc16010871220d
jossdev <josekandia42@gmail.com>
2023-04-08 00:05:34 +0200

    agrega efecto titulo en footer

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -69,21 +69,55 @@
     </p>
   </div>
 
-  <footer class="text-center text-white" style="background-color: #03001c;">
+  <footer class="text-center text-white pt-3" style="background-color: #03001c;">
     <ul class="row m-0">
-      <li class="col">Tecno phone</li>
-      <li class="col">Contacto
+      <li class="col-sm-12 col-lg-4 mb-5">
+        <h4 class="titulo-footer">Tecno Phone</h4>
+      </li>
+      <li class="col-sm-12 col-lg-4 mb-5">
+        <h4 class="titulo-footer">Contacto</h4>
         <ul class="m-3">
-          <li><span>tecnophone@gmail.com</span></li>
-          <li>3794551245</li>
-          <li>Av. Independencia 2345</li>
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-email.png" alt="" width="40" height="40">
+              tecnophone@gmail.com
+            </a>
+          </li>
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-telefono.png" alt="" width="40" height="40">
+              3794551245
+            </a>
+          </li>
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-map.png" alt="" width="40" height="40">
+              Av. Independencia 2345
+            </a>
+          </li>
         </ul>
       </li>
-      <li class="col">Redes sociales
-      <ul class="m-3">
-          <li>Facebook</li>
-          <li>Whatsapp</li>
-          <li>Instagram</li>
+      <li class="col-sm-12 col-lg-4 mb-5">
+        <h4 class="titulo-footer">Redes Sociales</h4>
+        <ul class="m-3">
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-facebook.png" alt="" width="50" height="50">
+              Facebook
+            </a>
+          </li>
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-whatsapp.png" alt="" width="45" height="45">
+              Whatsapp
+            </a>
+          </li>
+          <li class="p-2">
+            <a href="#" class="text-white">
+              <img src="./public/img/icons/icon-instagram.png" alt="" width="45" height="45">
+              Instagram
+            </a>
+          </li>
         </ul>
       </li>
     </ul>
