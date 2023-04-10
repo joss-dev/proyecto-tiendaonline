ec16297c2a6000eb0c3dc9164b3957777b6b121f
jossdev <josekandia42@gmail.com>
2023-04-10 10:41:13 +0200

    agrega favicon

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TecnoPhone</title>
-    
+    <link rel="shortcut icon" href="./public/img/favicon.png">
+
     <!-- estilos css -->
     <link rel="stylesheet" href="./public/css/myStyle.css">
     <link rel="stylesheet" href="./public/css/bootstrap.min.css">
