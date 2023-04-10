718d342c0963e88ed37a47bccc882d46ad19374f
jossdev <josekandia42@gmail.com>
2023-04-10 10:10:00 +0200

    agrega fonts

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -5,8 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TecnoPhone</title>
+    <!-- estilos css -->
     <link rel="stylesheet" href="./public/css/myStyle.css">
     <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
 </head>
 
 <body>
