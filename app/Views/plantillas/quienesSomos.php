fc98cbd4e16085944b4680677d7ca5c3c502cd9e
jossdev <josekandia42@gmail.com>
2023-04-20 11:30:56 +0200

    agrega imagen a quienesSomos

--- app/Views/plantillas/quienesSomos.php
+++ app/Views/plantillas/quienesSomos.php
@@ -1,10 +1,9 @@
 <div class="container">
-    <div class="row">
-        <div class="col align-self-center text-center m-5">
-            <img src="./public/img/icon.png" class="img-fluid" alt="..." height="200px" width="200px">
-            <h5 class="fw-bold mt-3 text-center">Tecno Phone</h5>
+    <div class="row d-flex justify-content-center">
+        <div class="col-12 m-5 ">
+        <img class="rounded mx-auto d-block img-fluid" src="./public/img/empresa/principales.jpg"  alt="">
         </div>
-        <div class="col text-center m-5 bg-propio p-3 border border-secondary">
+        <div class="col-12 text-center my-5 bg-propio p-3 border border-secondary">
             <p><strong>Tecno Phone</strong> inició sus actividades en el año 2011, fundada por jóvenes emprendedores argentinos.
                 El primer comercio se estableció en la ciudad de Corrientes, Argentina, y la actividad principal era la venta de celulares. En principio ofreciamos los productos en las zonas aledañas, pero con el tiempo fue creciendo y comenzó a diversificarse, incorporando todo tipo de marcas en celulares. Se crearon también cuentas en redes Sociales, seguidas de una página web, lo que permitió que la empresa se acerque más a sus clientes y promocione sus productos en Internet.</p>
             <p>Hoy somos una empresa con mas de 10 años de experiencia en la venta de disposivitos. Nos enfocamos en brindarle a nuestros clientes una excelente experiencia de compra, con los mejores precios y asesoramiento personalizado.</p>
