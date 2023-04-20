1768571714bc2199112f517a36f8a56bbf57ca6d
jossdev <josekandia42@gmail.com>
2023-04-20 10:54:22 +0200

    agrega nose

--- app/Views/plantillas/marcasPrincipales.php
+++ app/Views/plantillas/marcasPrincipales.php
@@ -1,13 +1,13 @@
-<div class="container-fluid">
-    <div class="row text-center">
-        <div class="col-4 mx-auto">
+<div class="container-fluid my-4">
+    <div class="row pt-2 text-center">
+        <div class="col-4 mx-auto efecto-opacidad">
             <img src="./public/img/icons/samsung.png" width="100" height="100" alt="">
         </div>
-        <div class="col-4 mx-auto">
-            <img src="./public/img/icons/samsung.png" width="100" height="100" alt="">
+        <div class="col-4 mx-auto efecto-opacidad">
+            <img src="./public/img/icons/applestore.png" width="100" height="100" alt="">
         </div>
-        <div class="col-4 mx-auto">
-            <img src="./public/img/icons/samsung.png" width="100" height="100" alt="">
+        <div class="col-4 mx-auto efecto-opacidad">
+            <img src="./public/img/icons/huawei.png" width="100" height="100" alt="">
         </div>
     </div>
 </div>
