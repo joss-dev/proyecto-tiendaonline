24b10a20cc9dd9367d975d64ab2e007dd722b9c4
jossdev <josekandia42@gmail.com>
2023-04-20 22:29:24 +0200

    agrega nose

--- app/Views/plantillas/quienesSomos.php
+++ app/Views/plantillas/quienesSomos.php
@@ -1,7 +1,28 @@
 <div class="container">
     <div class="row d-flex justify-content-center">
-        <div class="col-12 m-5 ">
-        <img class="rounded mx-auto d-block img-fluid" src="./public/img/empresa/principales.jpg"  alt="">
+        <div class="col-12 m-5  d-flex justify-content-center">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="max-width: 600px;">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img  src="./public/img/empresa/principales.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/img/empresa/tecnophone.jpg" class="d-block w-100 " alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/img/empresa/tiendaphone.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            
         </div>
         <div class="col-12 text-center my-5 bg-propio p-3 border border-secondary">
             <p><strong>Tecno Phone</strong> inició sus actividades en el año 2011, fundada por jóvenes emprendedores argentinos.
