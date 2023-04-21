549ad91728e6174d7ac6f43474da4ddbe5d2bbbf
Jmjoss <josekandia42@gmail.com>
2023-04-20 23:25:27 -0300

    agrega efecto escala

--- app/Views/plantillas/quienesSomos.php
+++ app/Views/plantillas/quienesSomos.php
@@ -1,13 +1,10 @@
 <div class="container">
-    <div class="row d-flex justify-content-center">
-        <div class="col-12 m-5  d-flex justify-content-center">
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="max-width: 600px;">
+    <div class="row d-flex justify-content-center ">
+        <div class="col-12 mt-5  d-flex justify-content-center ">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade border border-dark" data-bs-interval="3000" data-bs-ride="carousel" style="max-width: 600px;">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img  src="./public/img/empresa/principales.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./public/img/empresa/tecnophone.jpg" class="d-block w-100 " alt="...">
                     </div>
                     <div class="carousel-item">
                         <img src="./public/img/empresa/tiendaphone.jpg" class="d-block w-100" alt="...">
@@ -25,12 +22,13 @@
             
         </div>
         <div class="col-12 text-center my-5 bg-propio p-3 border border-secondary">
+            <h4 class="fw-bold">Nuestra Historia</h4>
             <p><strong>Tecno Phone</strong> inició sus actividades en el año 2011, fundada por jóvenes emprendedores argentinos.
                 El primer comercio se estableció en la ciudad de Corrientes, Argentina, y la actividad principal era la venta de celulares. En principio ofreciamos los productos en las zonas aledañas, pero con el tiempo fue creciendo y comenzó a diversificarse, incorporando todo tipo de marcas en celulares. Se crearon también cuentas en redes Sociales, seguidas de una página web, lo que permitió que la empresa se acerque más a sus clientes y promocione sus productos en Internet.</p>
             <p>Hoy somos una empresa con mas de 10 años de experiencia en la venta de disposivitos. Nos enfocamos en brindarle a nuestros clientes una excelente experiencia de compra, con los mejores precios y asesoramiento personalizado.</p>
-            <h4>Nuestra Misión</h4>
+            <h4 class="fw-bold">Nuestra Misión</h4>
             <p>Brindarles a nuestros clientes los mejores dispositivos diferenciándonos de la competencia.</p>
-            <h4>Nuestra Visión</h4>
+            <h4 class="fw-bold">Nuestra Visión</h4>
             <p>Convertirnos en una empresa líder en el mercado de la tecnología,ofreciendo a nuestros clientes la mejor calidad de atención al precio más conveniente.</p>
         </div>
     </div>
