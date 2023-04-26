11cd13c04fc8f478e6a8497c76f22871a9aa495b
Jmjoss <josekandia42@gmail.com>
2023-04-26 17:29:21 -0300

    arregla mapa

--- app/Views/plantillas/maps.php
+++ app/Views/plantillas/maps.php
@@ -1,6 +1,6 @@
 <div class="mt-4 text-center">
-    <h5> Nos encontras en: </h5>
-    <div class="d-flex">
-        <iframe class="pt-3 d-flex  align-items-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.949144811737!2d-58.81550648018766!3d-27.4784536139141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456b71d6422d07%3A0xd30b17b01c8a1316!2sAv.%20Independencia%202345%2C%20W3402%20Corrientes!5e0!3m2!1ses-419!2sar!4v1681953862558!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <h5 class="fw-bold"> Nos encontras en: </h5>
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.711319802574!2d-58.81443530000001!3d-27.4782453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456b71d6422d07%3A0xd30b17b01c8a1316!2sAv.%20Independencia%202345%2C%20W3402%20Corrientes!5e0!3m2!1ses-419!2sar!4v1682540677756!5m2!1ses-419!2sar" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
