c617ae42e059df21c84cc0aaab039f059fc75df3
jossdev <josekandia42@gmail.com>
2023-05-16 09:29:12 +0200

    agrega verificaciones de registro

--- app/Controllers/User_controller.php
+++ app/Controllers/User_controller.php
@@ -1,12 +1,15 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
 use App\Models\Consulta_model;
+use App\Models\Persona_model;
 
-class User_controller extends BaseController {
+class User_controller extends BaseController
+{
 
-    public function registrarse() {
+    public function registrarse()
+    {
         $data['titulo'] = 'Registrarse';
         echo view('plantillas/encabezado', $data);
         echo view('plantillas/nav');
@@ -14,7 +17,8 @@ class User_controller extends BaseController {
         echo view('plantillas/footer');
     }
 
-    public function login_view() {
+    public function login_view()
+    {
         $data['titulo'] = 'Iniciar Sesión';
         echo view('plantillas/encabezado', $data);
         echo view('plantillas/nav');
@@ -24,35 +28,37 @@ class User_controller extends BaseController {
 
 
 
-    public function registro_cliente() {
-
-    }
-
-    public function registrar_consulta() {
-        
+    public function registro_persona()
+    {
         $request = \Config\Services::request();
 
-        if($request->is('post')) {
+        if ($request->is('post')) {
             $rules = [
                 'nombre' => 'required',
-                'correo' => 'required|valid_email',
-                'mensaje' => 'required'
+                'apellido' => 'required',
+                'telefono' => 'required|numeric|is_natural_no_zero',
+                'correo' => 'required|valid_email|is_unique[personas.persona_correo]',
+                'pass' => 'required|min_length[8]',
+                'repass' => 'required|min_length[8]|matches[pass]'
             ];
 
             $validations = $this->validate($rules);
 
-            if($validations) {
+            if ($validations) {
                 $data = [
-                    'consulta_nombre' => $request->getPost('nombre'),
-                    'consulta_correo' => $request->getPost('correo'),
-                    'consulta_mensaje' => $request->getPost('mensaje')
+                    'persona_nombre' => $request->getPost('nombre'),
+                    'persona_apellido' => $request->getPost('apellido'),
+                    'persona_correo' => $request->getPost('correo'),
+                    'persona_telefono' => $request->getPost('telefono'),
+                    'persona_pass' => password_hash($request->getPost('pass'), PASSWORD_BCRYPT),
+                    'persona_estado' => 1
                 ];
-                
-                $registroConsulta = new Consulta_model();
+
+                $registroConsulta = new Persona_model();
                 $registroConsulta->insert($data);
 
                 return redirect()->to('contacto');
-            }else {
+            } else {
                 $data['validation'] = $this->validator;
                 $data['titulo'] = 'Contacto';
                 echo view('plantillas/encabezado', $data);
@@ -61,10 +67,45 @@ class User_controller extends BaseController {
                 echo view('plantillas/footer');
             }
         }
-    }    
-
-    public function login() {
-
     }
 
+    public function registrar_consulta()
+    {
+
+        $request = \Config\Services::request();
+
+        if ($request->is('post')) {
+            $rules = [
+                'nombre' => 'required',
+                'correo' => 'required|valid_email',
+                'mensaje' => 'required'
+            ];
+
+            $validations = $this->validate($rules);
+
+            if ($validations) {
+                $data = [
+                    'consulta_nombre' => $request->getPost('nombre'),
+                    'consulta_correo' => $request->getPost('correo'),
+                    'consulta_mensaje' => $request->getPost('mensaje')
+                ];
+
+                $registroConsulta = new Consulta_model();
+                $registroConsulta->insert($data);
+
+                return redirect()->to('contacto');
+            } else {
+                $data['validation'] = $this->validator;
+                $data['titulo'] = 'Contacto';
+                echo view('plantillas/encabezado', $data);
+                echo view('plantillas/nav');
+                echo view('plantillas/formContacto');
+                echo view('plantillas/footer');
+            }
+        }
+    }
+
+    public function login()
+    {
+    }
 }
---
+++ app/Models/Persona_model.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Consulta_model extends Model
+{
+    protected $table      = 'personas';
+    protected $primaryKey = 'id_persona';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['persona_nombre', 'persona_apellido', 'persona_telefono', "persona_correo", "persona_pass", "persona_estado"];
+
+    // Dates
+    protected $useTimestamps = false;// poner en true
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at'; //agregar a la tabla
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
