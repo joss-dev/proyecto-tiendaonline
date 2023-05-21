c4e534167ffb4b56dcf9f0ef110a0cd6bc162d7a
Jmjoss <josekandia42@gmail.com>
2023-05-21 10:52:00 -0300

    agrega setValue a registro

--- app/Views/plantillas/formRegistro.php
+++ app/Views/plantillas/formRegistro.php
@@ -5,7 +5,7 @@
     <?php $validation = \Config\Services::validation(); ?>
     <div class="mb-3">
         <label for="exampleInputText" for="nombre" class="form-label">Nombre</label>
-        <input type="text" name="nombre" class="form-control border border-dark" id="exampleInputText" aria-describedby="emailHelp" placeholder="Ingrese su nombre">
+        <input type="text" name="nombre" value="<?php echo set_value('nombre'); ?>" class="form-control border border-dark" id="exampleInputText" aria-describedby="emailHelp" placeholder="Ingrese su nombre">
         <?php if ($validation->getError('nombre')) { ?>
             <div class='alert alert-danger mt-2'>
                 <?= $error = $validation->getError('nombre'); ?>
@@ -14,7 +14,7 @@
     </div>
     <div class="mb-3">
         <label for="exampleInputText" for="apellido" class="form-label">Apellido</label>
-        <input type="text" name="apellido" class="form-control border border-dark" id="exampleInputText" aria-describedby="emailHelp" placeholder="Ingrese su Apellido">
+        <input type="text" name="apellido" value="<?php echo set_value('apellido'); ?>" class="form-control border border-dark" id="exampleInputText" aria-describedby="emailHelp" placeholder="Ingrese su Apellido">
         <?php if ($validation->getError('apellido')) { ?>
             <div class='alert alert-danger mt-2'>
                 <?= $error = $validation->getError('apellido'); ?>
@@ -23,7 +23,7 @@
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" for="correo" class="form-label">Correo electrónico</label>
-        <input type="email" name="correo" class="form-control border border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su correo">
+        <input type="email" name="correo" value="<?php echo set_value('correo'); ?>"  class="form-control border border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su correo">
         <?php if ($validation->getError('correo')) { ?>
             <div class='alert alert-danger mt-2'>
                 <?= $error = $validation->getError('correo'); ?>
@@ -32,7 +32,7 @@
     </div>
     <div class="mb-3">
         <label for="telefono" class="form-label">Telefono</label>
-        <input type="tel" name="telefono" class="form-control border border-dark" id="exampleInputText" aria-describedby="telHelp" placeholder="Ingrese su telefono">
+        <input type="tel" name="telefono" value="<?php echo set_value('telefono'); ?>" class="form-control border border-dark" id="exampleInputText" aria-describedby="telHelp" placeholder="Ingrese su telefono">
         <?php if ($validation->getError('telefono')) { ?>
             <div class='alert alert-danger mt-2'>
                 <?= $error = $validation->getError('telefono'); ?>
@@ -41,7 +41,7 @@
     </div>
     <div class="mb-3">
         <label for="exampleInputText" for="dni" class="form-label">Dni (Sin puntos)</label>
-        <input type="numeric" name="dni" class="form-control border border-dark" id="exampleInputNumeric" aria-describedby="numericHelp" placeholder="Ingrese su dni">
+        <input type="numeric" name="dni" value="<?php echo set_value('dni'); ?>" class="form-control border border-dark" id="exampleInputNumeric" aria-describedby="numericHelp" placeholder="Ingrese su dni">
         <?php if ($validation->getError('dni')) { ?>
             <div class='alert alert-danger mt-2'>
                 <?= $error = $validation->getError('dni'); ?>
