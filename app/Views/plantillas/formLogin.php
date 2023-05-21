af91096d2026e7a7761d6189ddfc3a8f04d9e1cf
Jmjoss <josekandia42@gmail.com>
2023-05-21 15:27:15 -0300

    agrega setValue a login

--- app/Views/plantillas/formLogin.php
+++ app/Views/plantillas/formLogin.php
@@ -1,36 +1,39 @@
-<div class="container mt-5 mb-5">
-    <h1 class="text-center">Iniciar Sesión</h1>
-    <?php echo form_open('login'); ?>
-    <?php $validation = \Config\Services::validation(); ?>
-    <?php if (session()->getFlashdata('mensajeVerif')) { ?>
-            <div class='alert alert-danger alert-dismissible fade show text-center py-3 my-3' role='alert' id='mensaje'>
-                <?= session()->getFlashdata('mensajeVerif'); ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="container d-flex justify-content-center p-2 my-5">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h1 class="text-center mb-3">Iniciar Sesión</h1>
+            <?php echo form_open('login'); ?>
+            <?php $validation = \Config\Services::validation(); ?>
+            <?php if (session()->getFlashdata('mensajeVerif')) { ?>
+                <div class='alert alert-danger alert-dismissible fade show text-center py-3 my-3' role='alert' id='mensaje'>
+                    <?= session()->getFlashdata('mensajeVerif'); ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php } ?>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" for="correo" class="form-label">Correo electrónico</label>
+                <input type="email" name="correo" value="<?php echo set_value('correo'); ?>" class="form-control border border-dark " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su correo">
+                <?php if ($validation->getError('correo')) { ?>
+                    <div class='alert alert-danger mt-2'>
+                        <?= $error = $validation->getError('correo'); ?>
+                    </div>
+                <?php } ?>
             </div>
-        <?php } ?>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" for="correo" class="form-label">Correo electrónico</label>
-        <input type="email" name="correo" value="<?php echo set_value('correo'); ?>" class="form-control border border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su correo">
-        <?php if ($validation->getError('correo')) { ?>
-            <div class='alert alert-danger mt-2'>
-                <?= $error = $validation->getError('correo'); ?>
+            <div class="mb-3">
+                <label for="exampleInputPassword" for="pass" class="form-label">Contraseña</label>
+                <input type="password" name="pass" class="form-control border border-dark" id="inputPassword" aria-describedby="emailHelp" placeholder="Ingrese su contraseña">
+                <?php if ($validation->getError('pass')) { ?>
+                    <div class='alert alert-danger mt-2'>
+                        <?= $error = $validation->getError('pass'); ?>
+                    </div>
+                <?php } ?>
             </div>
-        <?php } ?>
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword" for="pass" class="form-label">Contraseña</label>
-        <input type="password" name="pass" class="form-control border border-dark" id="inputPassword" aria-describedby="emailHelp" placeholder="Ingrese su contraseña">
-        <?php if ($validation->getError('pass')) { ?>
-            <div class='alert alert-danger mt-2'>
-                <?= $error = $validation->getError('pass'); ?>
+            <?php echo form_submit('login', 'Iniciar Sesión', "class='btn btn-success mb-4'"); ?>
+            <div class="text-center">
+                <h6 class="mb-3">¿ No tienes una cuenta ? Regístrate aqui en simples pasos.</h6>
+                <a class="btn btn-info" href="<?php echo base_url('registrarse'); ?>">Registrarse</a>
             </div>
-        <?php } ?>
+            <?php form_close(); ?>
+        </div>
     </div>
-    <?php echo form_submit('login', 'Iniciar Sesión', "class='btn btn-success mb-4'"); ?>
-    <div class="text-center">
-        <h6 class="mb-3">¿ No tienes una cuenta ? Regístrate aqui en simples pasos.</h6>
-        <a class="btn btn-info" href="<?php echo base_url('registrarse'); ?>">Registrarse</a>
-    </div>
-</div>
-<?php form_close(); ?>
 </div>
--- app/Views/plantillas/sectionDestacados.php
+++ app/Views/plantillas/sectionDestacados.php
@@ -2,7 +2,7 @@
     <h2 class="text-center m-5">Productos Destacados</h2>
     <ul class="row gy-5">
         <li class="col d-flex justify-content-center">
-            <div class="card text-center" style="width: 18rem;">
+            <div class="card card-hover text-center" style="width: 18rem;">
                 <img src="./public/img/ejemplos/iphone14.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Iphone 14 Pro Max 128GB</h5>
@@ -12,7 +12,7 @@
             </div>
         </li>
         <li class="col d-flex justify-content-center">
-            <div class="card text-center" style="width: 18rem;">
+            <div class="card card-hover text-center" style="width: 18rem;">
                 <img src="./public/img/ejemplos/samsungs23.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Samsung S23 Ultra 128GB</h5>
@@ -22,7 +22,7 @@
             </div>
         </li>
         <li class="col d-flex justify-content-center">
-            <div class="card text-center" style="width: 18rem;">
+            <div class="card card-hover text-center" style="width: 18rem;">
                 <img src="./public/img/ejemplos/iphone12.jpg" class="card-img-top" width="300" height="300" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Iphone 12 Pro Max 60GB</h5>
