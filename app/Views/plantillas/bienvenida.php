09c814ba73ea5ce050f03c0f5940fe28da75b5c4
Jmjoss <josekandia42@gmail.com>
2023-05-22 09:08:32 -0300

    agrega nombre a bienvenida

--- app/Views/plantillas/bienvenida.php
+++ app/Views/plantillas/bienvenida.php
@@ -1,3 +1,4 @@
+<?php $session = session();?>
 <div class="container-fluid mt-4 d-flex justify-content-center ">
         <div class="bg-propio mb-3 p-5 border border-5">
             <div class="row g-0">
@@ -7,7 +8,7 @@
                 </div>
                 <div class="col-md-6 text-center align-self-center">
                     <div class="card-body">
-                        <h3 class="card-title">Bienvenido</h3>
+                        <h3 class="card-title">Bienvenido, <?= $session->get('nombre'); ?></h3>
                         <p class="card-text">Somos una empresa con mas de <strong>10 años de experiencia en la venta de telefonos celulares.</strong>  Nos enfocamos en brindarle a nuestros clientes una excelente experiencia de compra, con los <strong>mejores precios</strong>  y asesoramiento personalizado. </p>
                         <a href="<?php echo base_url('quienesSomos');?>" class="btn btn-info">Leer mas</a>
                     </div>
