3e991c671162eec21e7499c6cab63c8f8096536a
jossdev <josekandia42@gmail.com>
2023-05-23 11:23:58 +0200

    kikuchi

--- app/Controllers/Admin_controller.php
+++ app/Controllers/Admin_controller.php
@@ -16,6 +16,46 @@ class Admin_controller extends BaseController
         echo view('plantillas/footer');
     }
 
+
+    public function registrar_producto()
+    {
+
+        $request = \Config\Services::request();
+
+        if ($request->is('post')) {
+            $rules = [
+                'nombreProducto' => 'required',
+                'marcaProducto' => 'required',
+                'descripcionProducto' => 'required',
+                'precioProducto' => 'required|alpha_numeric_space',
+                'imagenProducto' => 'required|valid_email',
+                'mensaje' => 'required'
+            ];
+
+            $validations = $this->validate($rules);
+
+            if ($validations) {
+                $data = [
+                    'consulta_nombre' => $request->getPost('nombre'),
+                    'consulta_correo' => $request->getPost('correo'),
+                    'consulta_mensaje' => $request->getPost('mensaje')
+                ];
+
+                $registroConsulta = new Consulta_model();
+                $registroConsulta->insert($data);
+
+                return redirect()->to('contacto')->with('Mensaje', 'Mensaje enviado correctamente, nos contactaremos a la brevedad.');
+            } else {
+                $data['validation'] = $this->validator;
+                $data['titulo'] = 'Contacto';
+                echo view('plantillas/encabezado', $data);
+                echo view('plantillas/nav');
+                echo view('plantillas/formContacto');
+                echo view('plantillas/footer');
+            }
+        }
+    }
+
 }
 
 ?>
