ecade6b6c08464cf73554d86d566d001035f4a52
Jmjoss <josekandia42@gmail.com>
2023-05-31 10:12:09 -0300

    arregla card

--- app/Views/plantillas/catalogo.php
+++ app/Views/plantillas/catalogo.php
@@ -3,12 +3,12 @@
     <ul class="row gy-5">
             <?php foreach ($productos as $row) { ?>
                 <li class="col d-flex justify-content-center mx-3">
-                    <div class="card text-center p-2" style="width: 18rem;">
-                        <img src="<?php echo base_url('public/img/ejemplos/' . $row['producto_imagen']); ?>" height="300" width="300" class="card-img-top" alt="...">
+                    <div class="card text-center p-2 card-hover" >
+                        <img src="<?php echo base_url('public/img/ejemplos/' . $row['producto_imagen']); ?>"  class="card-img-top card-image" alt="...">
                         <div class="card-body mt-2">
                             <h5 class="card-title my-3 "><?= $row['producto_nombre']; ?></h5>
-                            <span class="d-block fw-bold">$ <?= number_format($row['producto_precio'], 0, ',', '.'); ?></span>
-                            <a href="" class="btn btn-primary mt-4">Comprar</a>
+                            <span class="d-block card-price fw-bold">$ <?= number_format($row['producto_precio'], 0, ',', '.'); ?></span>
+                            <a href="" class="btn btn-primary card-button mt-4">Comprar</a>
                         </div>
                     </div>
                 </li>
