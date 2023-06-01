85814b0ba549cd8d005cefa19b898f24e7116080
jossdev <josekandia42@gmail.com>
2023-06-01 09:09:58 +0200

    quita estilos de card

--- app/Views/plantillas/catalogo.php
+++ app/Views/plantillas/catalogo.php
@@ -3,7 +3,7 @@
     <ul class="row gy-5">
             <?php foreach ($productos as $row) { ?>
                 <li class="col d-flex justify-content-center mx-3">
-                    <div class="card text-center p-2 card-hover" >
+                    <div class="card text-center p-2 card-hover" style="width: 18rem;">
                         <img src="<?php echo base_url('public/img/ejemplos/' . $row['producto_imagen']); ?>"  class="card-img-top card-image" alt="...">
                         <div class="card-body mt-2">
                             <h5 class="card-title my-3 "><?= $row['producto_nombre']; ?></h5>
